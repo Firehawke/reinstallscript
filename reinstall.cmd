b6cc4195f6ebe731ceb94061ffd8f32b12f95745
Firehawke <Firehawke@gmail.com>
2024-05-09 10:31:55 -0700

    A few package adjustments; e.g. add gping from scoop

--- reinstall.cmd
+++ reinstall.cmd
@@ -1,11 +1,23 @@
 @echo off
 rem Preliminary steps..
 rem Set up network shares
-net use h: \\filebox\emumedia /user:firehawke * /PERSISTENT:YES
-net use i: \\filebox\Emu-FH /user:firehawke * /PERSISTENT:YES
-net use p: \\filebox\fhstuff /user:firehawke * /PERSISTENT:YES
-net use v: \\filebox\avmedia /user:firehawke * /PERSISTENT:YES
-net use w: \\filebox\public /user:firehawke * /PERSISTENT:YES
+set /p "user=Enter NAS account username: "
+set /p "pass=Enter NAS account password: " 
+net use /delete h:
+net use /delete i:
+net use /delete p:
+net use /delete v:
+net use /delete w:
+net use /delete k:
+
+net use h: \\filebox\emumedia /user:%user% %pass% /PERSISTENT:YES
+net use i: \\filebox\Emu-FH /user:%user% %pass% /PERSISTENT:YES
+net use p: \\filebox\fhstuff /user:%user% %pass% /PERSISTENT:YES
+net use v: \\filebox\avmedia /user:%user% %pass% /PERSISTENT:YES
+net use w: \\filebox\public /user:%user% %pass% /PERSISTENT:YES
+net use k: \\filebox\plexripper /user:%user% %pass% /PERSISTENT:YES
+set pass=
+set user=
 
 rem Install gsudo first..
 winget install gerardog.gsudo 
@@ -22,10 +34,10 @@ pwsh /c "iwr -useb get.scoop.sh | iex"
 rem Now we can install the rest of the packages!
 
 rem Winget first..
-"C:\Program Files\gsudo\Current\gsudo.exe" winget install Doist.Todoist HeroicGamesLauncher.HeroicGamesLauncher 7zip.7zip OpenWhisperSystems.Signal Amazon.Kindle Audacity.Audacity AutoHotkey.AutoHotkey Codeusa.BorderlessGaming BurntSushi.ripgrep.GNU Discord.Discord voidtools.Everything AndreWiethoff.ExactAudioCopy GNU.Wget2 Git.Git GnuPG.GnuPG GnuPG.Gpg4win Gyan.FFmpeg HermannSchinagl.LinkShellExtension IrfanSkiljan.IrfanView rcmaehl.MSEdgeRedirect Microsoft.DevHome Microsoft.WindowsTerminal.Preview Mozilla.Firefox Mozilla.Thunderbird Insecure.Nmap Notepad++.Notepad++ OBSProject.OBSStudio Meta.Oculus JanDeDobbeleer.OhMyPosh Playnite.Playnite Plex.Plex SoftwareFreedomConservancy.QEMU Libretro.RetroArch Rufus.Rufus ScummVM.ScummVM Meltytech.Shotcut
+"C:\Program Files\gsudo\Current\gsudo.exe" winget install Doist.Todoist HeroicGamesLauncher.HeroicGamesLauncher 7zip.7zip OpenWhisperSystems.Signal Amazon.Kindle Audacity.Audacity AutoHotkey.AutoHotkey Codeusa.BorderlessGaming BurntSushi.ripgrep.GNU Discord.Discord voidtools.Everything AndreWiethoff.ExactAudioCopy GNU.Wget2 Git.Git GnuPG.GnuPG GnuPG.Gpg4win Gyan.FFmpeg HermannSchinagl.LinkShellExtension IrfanSkiljan.IrfanView rcmaehl.MSEdgeRedirect Microsoft.DevHome Microsoft.WindowsTerminal.Preview Mozilla.Firefox Mozilla.Thunderbird Insecure.Nmap Notepad++.Notepad++ OBSProject.OBSStudio Meta.Oculus JanDeDobbeleer.OhMyPosh Playnite.Playnite Plex.Plex SoftwareFreedomConservancy.QEMU Libretro.RetroArch Rufus.Rufus ScummVM.ScummVM Meltytech.Shotcut zyedidia.micro
 "C:\Program Files\gsudo\Current\gsudo.exe" winget install Valve.Steam Ubisoft.Connect RARLab.WinRAR Ferdium.Ferdium Balena.Etcher DupeGuru.DupeGuru Element.Element mIRC.mIRC WinSCP.WinSCP yt-dlp.yt-dlp Synology.DriveClient Microsoft.PowerShell KeePassXCTeam.KeePassXC Microsoft.DotNet.DesktopRuntime.7 Python.Launcher GitHub.cli Elgato.StreamDeck Foxit.FoxitReader Amazon.Games Logitech.GHUB Telegram.TelegramDesktop Microsoft.WindowsADK Microsoft.PowerToys Kitware.CMake Microsoft.ADKPEAddon Microsoft.OpenSSH.Beta calibre.calibre Google.Chrome tailscale.tailscale Oracle.VirtualBox Splashtop.SplashtopPersonal PlayStation.DualSenseFWUpdater VideoLAN.VLC JohnMacFarlane.Pandoc Microsoft.PowerShell Nvidia.PhysX namazso.OpenHashTab SteamGridDB.RomManager Microsoft.XNARedist 
 "C:\Program Files\gsudo\Current\gsudo.exe" winget install CodeSector.TeraCopy Microsoft.OpenJDK.11 dorssel.usbipd-win Microsoft.VisualStudioCode Python.Python.3.12 Microsoft.DotNet.DesktopRuntime.6 ElectronicArts.EADesktop Microsoft.DotNet.DesktopRuntime.8 CloudImperiumGames.RSILauncher KDE.Krita Logseq.Logseq WinDirStat.WinDirStat DigitalScholar.Zotero Twilio.Authy SyncTrayzor.SyncTrayzor mikf.gallery-dl GNU.Nano jftuga.less
-"C:\Program Files\gsudo\Current\gsudo.exe" winget install Nethack.Nethack GNU.Wget2 GNU.MidnightCommander sirredbeard.wslinternals Hugo.Hugo.Extended
+"C:\Program Files\gsudo\Current\gsudo.exe" winget install Nethack.Nethack GNU.Wget2 GNU.MidnightCommander sirredbeard.wslinternals Hugo.Hugo.Extended QmmpDevelopmentTeam.qmmp
 
 rem Scoop next..
 rem Since we used scoop export -c > scoopfile.json
